e4885bdbf2e8e66f03a9f3f01d5afb004bd36876
Abel Arana <abel.arana@hotmail.com>
2025-02-04 10:38:04 -0500

    Agregar opciones de selección por defecto en los campos de prioridad y estado del formulario de negocio

--- resources/views/livewire/crm/negocio-modal.blade.php
+++ resources/views/livewire/crm/negocio-modal.blade.php
@@ -59,7 +59,8 @@
                             class="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                     </div>
                     <div class="col-span-6 sm:col-span-3">
-                        <x-select-input wire:model.live='negocioForm.priority' for='priority' label='Prioridad'>
+                        <x-select-input wire:model.live='negocioForm.priority' for='priority' label='Prioridad'>      
+                            <option value="">--Select--</option>                 
                             <option value="ALTA">ALTA</option>
                             <option value="MEDIA">MEDIA</option>
                             <option value="BAJA">BAJA</option>
@@ -76,6 +77,7 @@
                     </div>
                     <div class="col-span-6 sm:col-span-3">
                         <x-select-input wire:model.live='negocioForm.stage' for='stage' label='Estado'>
+                            <option value="">--Select--</option>
                             <option value="NUEVO">NUEVO</option>
                             <option value="COTIZADO">COTIZADO</option>
                             <option value="GANADO">GANADO</option>
